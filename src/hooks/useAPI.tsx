27cb5bad6874a964b6b8fde172d089c3b77ccf11
arianna choza <unachoza@gmail.com>
2024-05-09 18:10:38 -0400

    fetching hook

---
+++ src/hooks/useAPI.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export const useAPI = (url: string) => {
+	const [data, setData] = useState<any>([]);
+	const [error, setError] = useState<Boolean>(false);
+	const [loading, setLoading] = useState<Boolean>(false);
+
+	const fetchUserData = async () => {
+		try {
+			setLoading(true);
+			const data = await axios.get(url);
+			setData(data);
+		} catch (e) {
+			setError(true);
+			console.log({ e });
+		} finally {
+			setLoading(false);
+		}
+	};
+	fetchUserData();
+
+	return { data, error, loading };
+};
